921f0f1203e88a953741aa128086cf73123d6cc7
Pradeep Kaswan <pradeepkaswan7@gmail.com>
2024-01-16 19:14:29 +0530

    feat: chip assignement

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,141 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useState, useEffect, ChangeEvent, KeyboardEvent } from "react";
+import { User, userData } from "@/data/user-data";
+import ViewContainer from "@/components/layouts/view-container";
+import Image from "next/image";
+
+const initialUsers = userData;
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [users, setUsers] = useState<User[]>(initialUsers);
+  const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<User[]>(initialUsers);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [pressCount, setPressCount] = useState(0);
+  const [lastSelectedIndex, setLastSelectedIndex] = useState<null | number>(
+    null
+  );
+
+  useEffect(() => {
+    setFilteredUsers(users);
+  }, [users]);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Backspace") {
+      setPressCount((prevCount) => prevCount + 1);
+      console.log("Presscount", pressCount);
+      if ((pressCount + 1) % 2 === 0) {
+        setSelectedUsers((prevUsers) => prevUsers.slice(0, -1));
+      } else {
+        setLastSelectedIndex(selectedUsers.length - 1);
+      }
+    }
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const value = e.target.value;
+    setInputValue(value);
+
+    if (value.trim() === "") {
+      setFilteredUsers(users);
+    } else {
+      const filtered = users.filter((user) =>
+        user.name.toLowerCase().includes(value.toLowerCase())
+      );
+      setFilteredUsers(filtered);
+    }
+
+    setDropdownOpen(value.trim() !== "");
+  };
+
+  const handleUserAction = (user: User, action: "add" | "remove") => {
+    if (action === "add") {
+      setSelectedUsers((prevSelectedUsers) => [...prevSelectedUsers, user]);
+      setUsers((prevUsers) => prevUsers.filter((u) => u.id !== user.id));
+      setFilteredUsers((prevFilteredUsers) =>
+        prevFilteredUsers.filter((u) => u.id !== user.id)
+      );
+    } else {
+      const updatedUserList = selectedUsers.filter((u) => u.id !== user.id);
+      setUsers((prevState) => [...prevState, user]);
+      setSelectedUsers(updatedUserList);
+    }
+
+    setInputValue("");
+    setDropdownOpen(true);
+  };
+
+  const handleRemove = (removedUser: User) => {
+    handleUserAction(removedUser, "remove");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <ViewContainer className="relative flex flex-col items-center gap-10 m-4">
+      <h1 className="text-4xl font-bold text-blue-600">Pick Users</h1>
+      <div className="wrapper w-full border-b-4 border-blue-600 bg-transparent pt-2 pb-2 font-sans ">
+        <ul className="flex flex-wrap gap-6">
+          {selectedUsers.map((user, index) => (
+            <li
+              key={user.id}
+              className={`flex items-center bg-gray-300 rounded-full gap-4 ${
+                index === lastSelectedIndex ? "bg-gray-400" : "bg-gray-300"
+              }`}
+            >
+              <Image
+                className="rounded-full h-8 w-8"
+                src={user.avatar}
+                height={10}
+                width={10}
+                alt="avatar"
+              />
+              <span>{user.name}</span>
+              <button className="pr-3" onClick={() => handleRemove(user)}>
+                &#x2715;
+              </button>
+            </li>
+          ))}
+          <div>
+            <input
+              className="outline outline-0"
+              type="text"
+              placeholder="Add new user..."
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+              onClick={() => setDropdownOpen(true)}
             />
-          </a>
-        </div>
+
+            {isDropdownOpen && (
+              <ul className="bg-white border rounded shadow-2xl max-h-80 overflow-auto absolute">
+                {filteredUsers.map((user) => (
+                  <li
+                    key={user.id}
+                    className="px-6 font-sans text-sm py-2 cursor-pointer transition duration-300 hover:bg-gray-200 flex items-center space-x-4"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleUserAction(user, "add");
+                    }}
+                  >
+                    <Image
+                      src={user.avatar}
+                      height={50}
+                      width={50}
+                      className="rounded-full"
+                      alt="avatar"
+                    />
+                    <div>{user.name}</div>
+                    <div className="text-gray-500">{user.email}</div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </ul>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </ViewContainer>
+  );
 }
---
+++ data/user-data.ts
@@ -0,0 +1,69 @@
+export interface User {
+  id: number;
+  avatar: string;
+  name: string;
+  email: string;
+}
+
+export const userData: User[] = [
+  {
+    id: 1,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Letitia George",
+    email: "letitia.george@example.com",
+  },
+  {
+    id: 2,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Soham Newman",
+    email: "soham.newman@example.com",
+  },
+  {
+    id: 3,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Denise Hall",
+    email: "deni.hall@example.com",
+  },
+  {
+    id: 4,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Tracy Bing",
+    email: "tracy.bing@example.com",
+  },
+  {
+    id: 5,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Ken Alvarez",
+    email: "ken.alvarez@example.com",
+  },
+  {
+    id: 6,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Calvin Jones",
+    email: "calvin.jones@example.com",
+  },
+  {
+    id: 7,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Bradley Wells",
+    email: "bradley.wells@example.com",
+  },
+  {
+    id: 8,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Brad Grant",
+    email: "brad.grant@example.com",
+  },
+  {
+    id: 9,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Tim Hall",
+    email: "tim.hall@example.com",
+  },
+  {
+    id: 10,
+    avatar: "https://avatars.githubusercontent.com/pradeepkaswan",
+    name: "Tamara Cox",
+    email: "tamara.cox@example.com",
+  },
+];
